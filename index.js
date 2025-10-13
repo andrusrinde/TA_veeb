6c9810a0809dfbb8f226506f00247f4950f8af3e
Andrus Rinde <andrusrinde@users.noreply.github.com>
2025-10-13 13:14:54 +0300

    Loodud marsuudid koos kontrolleritega. Kasutamae asünkroonset programmeerimist

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const express = require("express");
 const fs = require("fs");
-//päringu lahtiharutaja POST jaoks
 const bodyparser = require("body-parser");
-//SQL andmebaasi moodul
-const mysql =require("mysql2");
+//const mysql = require("mysql2/promise");
 const dateEt = require("./src/dateTimeET");
-const dbInfo = require("../../../../vp2025config");
+//const dbInfo = require("../../../../vp2025config");
 const textRef = "public/txt/vanasonad.txt";
-//käivitan express.js funktsiooni ja annan talle nimeks "app"
 const app = express();
-//määran veebilehtede mallide renderdamise mootori
 app.set("view engine", "ejs");
-//määran ühe päris kataloogi avalikult kättesaadavaks
 app.use(express.static("public"));
-//parsime päringu URL-i, lipp false, kui ainult tekst ja true, kui muid andmeid ka
 app.use(bodyparser.urlencoded({extended: false}));
 
-//loon andmebaasiühenduse
-const conn = mysql.createConnection({
-	host: dbInfo.configData.host,
-	user: dbInfo.configData.user,
-	password: dbInfo.configData.passWord,
-	database: "if25_inga_petuhhov_TA"
-});
-
 app.get("/", (req, res)=>{
-	//res.send("Express.js läks käima ja serveerib veebi!");
 	res.render("index");
 });
 
@@ -39,7 +24,6 @@ app.get("/vanasonad", (req, res)=>{
 	let folkWisdom = [];
 	fs.readFile(textRef, "utf8", (err, data)=>{
 		if(err){
-			//kui tuleb viga, siis ikka väljastame veebilehe, liuhtsalt vanasõnu pole ühtegi
 			res.render("genericlist", {heading: "Valik Eesti vanasõnu", listData: ["Ei leidnud ühtegi vanasõna!"]});
 		}
 		else {
@@ -55,7 +39,6 @@ app.get("/regvisit", (req, res)=>{
 
 app.post("/regvisit", (req, res)=>{
 	console.log(req.body);
-	//avan tekstifaili kirjutamiseks sellisel moel, et kui teda pole, luuakse (parameeter "a")
 	fs.open("public/txt/visitlog.txt", "a", (err, file)=>{
 		if(err){
 			throw(err);
@@ -79,7 +62,6 @@ app.get("/visitlog", (req, res)=>{
 	let listData = [];
 	fs.readFile("public/txt/visitlog.txt", "utf8", (err, data)=>{
 		if(err){
-			//kui tuleb viga, siis ikka väljastame veebilehe, liuhtsalt vanasõnu pole ühtegi
 			res.render("genericlist", {heading: "Registreeritud külastused", listData: ["Ei leidnud ühtegi külastust!"]});
 		}
 		else {
@@ -93,46 +75,8 @@ app.get("/visitlog", (req, res)=>{
 	});
 });
 
-app.get("/Eestifilm", (req, res)=>{
-	res.render("eestifilm");
-});
-
-app.get("/Eestifilm/inimesed", (req, res)=>{
-	const sqlReq = "SELECT * FROM person";
-	conn.execute(sqlReq, (err, sqlres)=>{
-		if(err){
-			throw(err);
-		}
-		else {
-			console.log(sqlres);
-			res.render("filmiinimesed", {personList: sqlres});
-		}
-	});
-	//res.render("filmiinimesed");
-});
-
-app.get("/Eestifilm/filmiinimesed_add", (req, res)=>{
-	res.render("filmiinimesed_add", {notice: "Ootan sisestust"});
-});
-
-app.post("/Eestifilm/filmiinimesed_add", (req, res)=>{
-	console.log(req.body);
-	//kas andmed on olemas
-	if(!req.body.firstNameInput || !req.body.lastNameInput || !req.body.bornInput || req.body.bornInput >= new Date()){
-	  res.render("filmiinimesed_add", {notice: "Osa andmeid oli puudu või ebakorrektsed"});
-	}
-	else {
-		let sqlReq = "INSERT INTO person (first_name, last_name, born, deceased) VALUES (?,?,?,?)";
-		conn.execute(sqlReq, [req.body.firstNameInput, req.body.lastNameInput, req.body.bornInput, req.body.deceasedInput], (err, sqlres)=>{
-			if(err){
-				res.render("filmiinimesed_add", {notice: "Andmete salvestamine ebaõnnestus"});
-			}
-			else {
-				res.render("filmiinimesed_add", {notice: "Andmed salvestatud"});
-			}
-		});
-		
-	}
-});
+//Eesti filmi marsruudid
+const eestifilmRouter = require("./routes/eestifilmRoutes");
+app.use("/Eestifilm", eestifilmRouter);
 
 app.listen(5100);
---
+++ routes/eestifilmRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+//kontrollerid
+const {
+	eestifilm,
+	inimesed,
+	inimesedAdd,
+	inimesedAddPost} = require("../controllers/eestifilmControllers");
+
+router.route("/").get(eestifilm);
+
+router.route("/inimesed").get(inimesed);
+
+router.route("/filmiinimesed_add").get(inimesedAdd);
+
+router.route("/filmiinimesed_add").post(inimesedAddPost);
+
+module.exports = router;
+
+
+
+
+
